064d55d73a741662dfaa681f20d86f374d21075d
Benjamin Dahl <ben.dahl@mail.com>
2015-11-04 15:45:23 -0800

    Initial Commit

---
+++ Collatz Conjecture/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Collatz Conjecture
+//
+//  Created by user on 11/4/15.
+//  Copyright (c) 2015 Ben Dahl. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
