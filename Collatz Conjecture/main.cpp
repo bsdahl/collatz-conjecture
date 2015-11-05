285b99d5edad5e211190e75db2a4f10f563f2349
Benjamin Dahl <ben.dahl@mail.com>
2015-11-04 16:46:21 -0800

    First commit

--- Collatz Conjecture/main.cpp
+++ Collatz Conjecture/main.cpp
@@ -7,9 +7,51 @@
 //
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+// returns the number of iterations in collatz conjucture
+int collatz(unsigned long long int n);
+
+int main() {
+    int num;
+    pair<int,int> numPair, maxIter;
+    vector<pair<int,int>> numIter;
+    
+    cout << "Collatz Conjecture" << endl;
+    
+    cout << "Enter an int: ";
+    cin >> num;
+    
+    cout << "Number\t:\tIterations" << endl;
+    for (int i = 1; i <= num; i++)
+    {
+        numPair = pair<int,int>(collatz(i),i);
+        numIter.push_back(numPair);
+        cout << i << "\t:\t" << numIter[i-1].first<< endl;
+    }
+    
+    maxIter = * max_element(numIter.begin(),numIter.end());
+    
+    cout << endl << "Value with Max number of iterations: " << maxIter.second << "\t:\t" << maxIter.first << endl;
+    
+    
     return 0;
+}
+
+
+int collatz(unsigned long long int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    if (n%2 == 0)
+        n /= 2;
+    else
+        n = n * 3 + 1;
+    
+    return 1 + collatz(n);
 }
